4534a3b35ac6df732976c38e537feda6935eca98
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-02 03:11:59 +0000

    finish?

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,19 +6,19 @@ class RelationshipsController < ApplicationController
     redirect_to request.referer
   end
 
-　#フォロー外すとき
+#フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
-　#フォロー一覧
+#フォロー一覧
   def followings
     user = User.find(params[:user_id])
     @user = user.followings
   end
 
-  ＃フォロワー一覧
+  #フォロワー一覧
   def followers
     user = User.find(params[:user_id])
     @user = user.followers
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
-  end
-
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
-  end
-
-  def following?(user)
-    followings.include?(user)
-  end
-
   def show
     @user = User.find(params[:id])
     @books = @user.books
